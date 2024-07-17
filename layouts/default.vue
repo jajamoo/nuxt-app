f88652d43d71538ab3e3b59b9ba7d20eb127763b
Moe Kahn <jajamoo@gmail.com>
2024-07-16 21:06:25 -0400

    styling - dark mode via tailwind css

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,6 +14,8 @@
 </template>
 
 <script setup>
+const colorMode = useColorMode();
+console.log(colorMode.preference)
 useHead({
   titleTemplate: '%s - Moe Kahn',
 })
@@ -22,5 +24,9 @@ useHead({
 <style>
 body {
   font-family: Monaco, Monaco;
+}
+
+body {
+  @apply bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-300
 }
 </style>
